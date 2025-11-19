bcc3398d2cef9267f010728c2238d96445a67ad5
samueldm <samueldimkpa20@gmail.com>
2025-11-19 12:14:33 +0100

    Remove .env and secret

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import cors from "cors";
 import createConnection from "./models/db.js";
 
 // Import tables
-import { createMarketPriceTable } from "./models/market_price.model.js";
+//import { createMarketPriceTable } from "./models/market_price.model.js";
+import { createTransactionTable } from "./models/transaction.model.js";
 
 // Import end points
-import marketPriceRouter from "./modules/market_price/market_price.route.js";
+//import marketPriceRouter from "./modules/market_price/market_price.route.js";
+import transactionrouter from "./modules/Transactions/Transaction.route.js";
+
 
 await createConnection();
-await createMarketPriceTable();
+//await createMarketPriceTable();
+await createTransactionTable();
 
 dotenv.config();
 const app = express();
@@ -18,7 +22,8 @@ app.use(cors());
 app.use(express.json());
 
 // endpoints
-app.use("/api/market-price", marketPriceRouter);
+//app.use("/api/market-price", marketPriceRouter);
+app.use("/api/transaction", transactionrouter);
 
 const PORT = process.env.PORT;
 app.get("/api/test", (req, res) => {
